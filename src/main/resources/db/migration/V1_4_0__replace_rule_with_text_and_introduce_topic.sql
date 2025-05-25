26197cd25713a5b29ec1bf5785408b0a6aef74a8
Alessandro Festa <agrfesta@gmail.com>
2025-05-26 01:45:16 +0200

    issue #53: merged flyway scripts

--- src/main/resources/db/migration/V1_4_0__replace_rule_with_text_and_introduce_topic.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE btm.rules_bits RENAME TO text_bits;
-ALTER TABLE btm.rules_embeddings RENAME TO embeddings;
-ALTER TABLE btm.embeddings RENAME CONSTRAINT fk_rules_bits TO fk_text_bit;
-ALTER TABLE btm.embeddings RENAME COLUMN rule_bit_id TO text_bit_id;
-
-CREATE TYPE topic_enum AS ENUM ('RULE', 'LORE');
-
-ALTER TABLE btm.text_bits
-ADD COLUMN topic topic_enum NOT NULL DEFAULT 'RULE';
